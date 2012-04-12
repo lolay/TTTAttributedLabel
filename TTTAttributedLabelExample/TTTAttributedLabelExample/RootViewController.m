953f4a3aea486d870d5dd48dc2aba0c04f4be6cc
GaryRudolph <GaryRudolph@mac.com>
2012-04-12 15:05:11 -0700

    Updated for license compliance

--- TTTAttributedLabelExample/TTTAttributedLabelExample/RootViewController.m
+++ TTTAttributedLabelExample/TTTAttributedLabelExample/RootViewController.m
@@ -1,6 +1,7 @@
 // RootViewController.m
 //
 // Copyright (c) 2011 Mattt Thompson (http://mattt.me)
+// Copyright (c) 2012 Lolay, Inc.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
